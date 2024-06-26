5cfeefb0791a196757e1b90a0c1914926073aef6
Mzknkung53 <chatchai_cha@cmu.ac.th>
2024-06-26 20:41:51 +0700

    Question 6
    question 6.1-6.6

--- main.js
+++ main.js
@@ -1,40 +1,28 @@
-const { createApp, ref } = Vue
+const { createApp, ref } = Vue;
 createApp({
-    setup(){
-        const product = ref('Boots')
-        const image = ref('./assets/images/socks_green.jpg')
-        const description = ref('This is boots')
-        const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
-        const inStock = ref(false)
-        const inventory = ref(7)
-        const onsale = ref(true)
-        const details = ref([
-            '50% cotton',
-            '30% wool',
-            '20% polyester'
-        ])
+    setup() {
+        // Attributes
+        const product = ref("Boots");
+        const description = ref("Wears on both feet, keeps you warm");
+        const image = ref("./assets/images/socks_green.jpg");
+        const inStock = ref(true);
+        const inventory = ref(100);
+        const link = ref("https://www.camt.cmu.ac.th/");
+        const sale = ref(false);
+        const details = ref(["50% cotton", "30% wool", "20% polyester"]);
         const variants = ref([
-            {id: 2234 , color: 'green'},
-            {id: 2235 , color: 'blue'}
-
-        ])
-        const sizes = ref([
-            'S',
-            'M',
-            'L'
-        ])
-        return{
-            product,
-            description,
-            image,
-            link,
-            inStock,
-            inventory,
-            onsale,
-            details,
-            variants,
-            sizes
-
+            { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
+            { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
+        ]);
+        const sizes = ref(["S", "M", "L"]);
+        const cart = ref(0);
+        function add_to_cart() {
+            cart.value++;
         }
-    }
-}).mount('#app')
+        function update_image(variant_image) {
+            image.value = variant_image;
+        }
+        // return
+        return { product, description, image, link, inStock, inventory, sale, details, variants, sizes, cart, add_to_cart, update_image };
+    },
+}).mount("#app");
