e7124e9bac1f99c25adce346e7993e7fb0a3dfb9
Mzknkung53 <chatchai_cha@cmu.ac.th>
2024-06-26 20:12:24 +0700

    Question 3
    3.1-3.6

--- main.js
+++ main.js
@@ -2,9 +2,14 @@ const { createApp, ref } = Vue
 createApp({
     setup(){
         const product = ref('Boots')
-        const description = ref('Description')
+        const image = ref('./assets/images/socks_green.jpg')
+        const description = ref('description')
+        const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
         return{
-            product,description
+            product,
+            description,
+            image,
+            link
         }
     }
 }).mount('#app')
