6ea4090bdc209ea842e3437e911ecbe0eac212fc
Mzknkung53 <chatchai_cha@cmu.ac.th>
2024-06-26 20:20:42 +0700

    Question 4
    4.1-4.9

--- main.js
+++ main.js
@@ -3,13 +3,19 @@ createApp({
     setup(){
         const product = ref('Boots')
         const image = ref('./assets/images/socks_green.jpg')
-        const description = ref('description')
+        const description = ref('This is boots')
         const link = ref('https://www.camt.cmu.ac.th/index.php/th/')
+        const inStock = ref(false)
+        const inventory = ref(7)
+        const onsale = ref(true)
         return{
             product,
             description,
             image,
-            link
+            link,
+            inStock,
+            inventory,
+            onsale
         }
     }
 }).mount('#app')
