b9d01646e083adcdfd2e8407bbb4be63ce8cbebf
Mzknkung53 <chatchai_cha@cmu.ac.th>
2024-06-26 20:29:19 +0700

    question 5
    question 5.1-5.5

--- main.js
+++ main.js
@@ -8,6 +8,21 @@ createApp({
         const inStock = ref(false)
         const inventory = ref(7)
         const onsale = ref(true)
+        const details = ref([
+            '50% cotton',
+            '30% wool',
+            '20% polyester'
+        ])
+        const variants = ref([
+            {id: 2234 , color: 'green'},
+            {id: 2235 , color: 'blue'}
+
+        ])
+        const sizes = ref([
+            'S',
+            'M',
+            'L'
+        ])
         return{
             product,
             description,
@@ -15,7 +30,11 @@ createApp({
             link,
             inStock,
             inventory,
-            onsale
+            onsale,
+            details,
+            variants,
+            sizes
+
         }
     }
 }).mount('#app')
