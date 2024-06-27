f7d84c4bd71884455c2896a3c37fe910d6db6ea0
Mzknkung53 <chatchai_cha@cmu.ac.th>
2024-06-27 21:38:19 +0700

    Question 10.5

--- main.js
+++ main.js
@@ -5,8 +5,15 @@ const app = createApp({
         const cart = ref([])
         const premium = ref(true)
         const details = ref(["50% cotton", "30% wool", "20% polyester"]);
-        function updateCart(id) {
-            cart.value.push(id)
+        function updateCart(_id) {
+            for (var i = 0; i < cart.value.length; i++) {
+                if(cart.value[i].id === _id){
+                    cart.value[i].amount++;
+                    return;
+                }
+            }
+            cart.value.push({id:_id, amount: 1})
+            return;
         }
         return {
             cart,
