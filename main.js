733fc9cf46519220081aa9858c5befe144203e79
Mzknkung53 <chatchai_cha@cmu.ac.th>
2024-06-27 15:27:55 +0700

    Question 9.3-9.4

--- main.js
+++ main.js
@@ -1,10 +1,12 @@
 const { createApp, ref, computed } = Vue;
 
-createApp({
+const app = createApp({
     setup () {
         const cart = ref(0)
         return {
             cart
         }
     }
-}).mount("#app");
+})
+app.component('product-display', productDisplay)
+app.mount('#app')
