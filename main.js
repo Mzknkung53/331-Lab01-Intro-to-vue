23b2d2222943db21beb79bd9128f7f31fe7062fe
Mzknkung53 <chatchai_cha@cmu.ac.th>
2024-06-27 13:40:51 +0700

    Question 8
    Question 8.1-8.4

--- main.js
+++ main.js
@@ -1,21 +1,30 @@
-const { createApp, ref } = Vue;
+const { createApp, ref, computed } = Vue;
 createApp({
     setup() {
         // Attributes
         const product = ref("Boots");
+        const brand = ref("SE 331")
         const description = ref("Wears on both feet, keeps you warm");
-        const image = ref("./assets/images/socks_green.jpg");
-        const inStock = ref(true);
         const inventory = ref(100);
         const link = ref("https://www.camt.cmu.ac.th/");
         const sale = ref(false);
         const details = ref(["50% cotton", "30% wool", "20% polyester"]);
         const variants = ref([
-            { id: 2234, color: "green", image: "./assets/images/socks_green.jpg" },
-            { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg" },
-        ]);
+            { id: 2234, color: "green", image: "./assets/images/socks_green.jpg", quantity: 50},
+            { id: 2235, color: "blue", image: "./assets/images/socks_blue.jpg", quantity: 0},
+        ])
+        const image = computed(() => {
+            return variants.value[selectedVariant.value].image
+        })
+        const inStock = computed(() => {
+            return variants.value[selectedVariant.value].quantity
+        })
+        const selectedVariant = ref(0)
         const sizes = ref(["S", "M", "L"]);
         const cart = ref(0);
+        const title = computed(() =>{
+            return brand.value + ' ' + product.value
+        })
         function add_to_cart() {
             cart.value++;
         }
@@ -25,7 +34,10 @@ createApp({
         function toggle_in_stock() {
             inStock.value = !inStock.value;
         }
+        function updateVariant(index){
+            selectedVariant.value = index;
+        }
         // return
-        return { product, description, image, link, inStock, inventory, sale, details, variants, sizes, cart, add_to_cart, update_image, toggle_in_stock };
+        return { title, description, image, link, inStock, inventory, sale, details, variants, sizes, cart, add_to_cart, update_image, toggle_in_stock, updateVariant };
     },
 }).mount("#app");
